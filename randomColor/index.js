3d4b5fdd93ee1057101467d0b7059bbfe6eb375e
quangkhuat <quangkhuat1995@gmail.com>
2020-07-07 21:15:20 +0700

    rewrite ES6 style, display lastest color when reload, change, delete

--- randomColor/index.js
+++ randomColor/index.js
@@ -1,21 +1,18 @@
 //Create array colors to take random color
 const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 /*VARIABLE*/
+const choice = document.getElementById("mode"); //select
 // used to create random color
 const color = document.getElementById("color");
 const colorPri = document.getElementById("colorPri");
 const colorSec = document.getElementById("colorSec");
-//distinguish each content (mode) by data-mode
-const contentGroup = document.querySelectorAll(".content");
 const collectList = document.querySelectorAll(".collection__list"); //ul
-const choice = document.getElementById("mode"); //select
-//To add "create color" or "add to collection" events
+const contentGroup = document.querySelectorAll(".content");
 const btns = document.querySelectorAll("button.btn");
 
-/*Event*/
+/*EVENTs*/
 choice.addEventListener("change", chooseMode);
 document.addEventListener("DOMContentLoaded", getLocalColor);
-
 /*CHANGE BACKGROUND COLOR AND ADD TO COLLECTION ON CLICK*/
 btns.forEach((btn) =>
   btn.addEventListener("click", function (e) {
@@ -44,9 +41,15 @@ btns.forEach((btn) =>
         colorSec.innerText,
         btn
       );
+      // lastNormal = colorArr.pop();
+      // lastLinear = [colorPriArr.pop(), colorSecArr.pop()];
+      // console.log(lastNormal);
+      // console.log(lastLinear);
     }
   })
 );
+let lastNormal = null;
+let lastLinear = [];
 
 //generateColor function
 //@Output Array of color [hex, hexPri, hexSec]
@@ -68,62 +71,64 @@ function generateColor() {
  *        colorSec: string, color of Linear mode
  *@output: create element tag*/
 function addColor(elem) {
-  //create div tag
-  let div = document.createElement("div");
-  div.classList.add("collection__group");
-  //create li tag
-  let li1 = document.createElement("li");
-  li1.classList.add("color__name");
-
-  //create button trash tag
-  let button_del = document.createElement("button");
-  button_del.setAttribute("class", "btn btn-trash");
-  button_del.setAttribute("title", "Delete");
-  //create button demo
-  let button_demo = document.createElement("button");
-  button_demo.setAttribute("class", "btn btn-use");
-  button_demo.setAttribute("title", "Demo this color");
-
-  //Add tags together
-  div.appendChild(li1);
-  // div.appendChild(li2);
-  div.appendChild(button_del);
-  div.appendChild(button_demo);
-  //create li2 tag(seccondary color)
-  let li2 = document.createElement("li");
+  /**
+   * test
+   */
+  let contentHTML = `
+  <div class="collection__group">
+    ${displayLiTag(elem)}
+    <button class="btn btn-trash" title="Delete" data-target=${
+      elem.dataset.target
+    }></button>
+    <button class="btn btn-use" title="Demo this color" data-target=${
+      elem.dataset.target
+    }></button>
+  </div>
+  `;
+  // TODO: data-target cua btn xoa, demo
   switch (elem.dataset.target) {
     case "normal":
-      li1.textContent = color.innerText;
-      //khong add class cho li2 de width=0 (li1 co flex:1)
-      li2.textContent = "";
-      //change bg-color of li
-      li1.style.background = color.innerText;
-      collectList[0].appendChild(div);
+      collectList[0].innerHTML += contentHTML;
       break;
     case "linear":
-      li1.textContent = colorPri.innerText;
-      li2.textContent = colorSec.innerText;
-      li2.classList.add("color__name");
-      li1.style.background = colorPri.innerText;
-      li2.style.background = colorSec.innerText;
-      button_demo.setAttribute("data-target", "linear");
-      button_del.setAttribute("data-target", "linear");
-      collectList[1].appendChild(div);
+      collectList[1].innerHTML += contentHTML;
       break;
   }
-  // add li2 to div
-  div.insertBefore(li2, button_del);
 }
+function displayLiTag(elem) {
+  let liTag = "";
+  switch (elem.dataset.target) {
+    case "normal":
+      //if add class for the 2nd <li> will cause unwanted display style
+      liTag = `
+      <li style="background:${color.innerText}" class="color__name">${color.innerText}</li>
+      <li></li>
+      `;
+      break;
+    case "linear":
+      liTag = `
+      <li style="background:${colorPri.innerText}" class="color__name">${colorPri.innerText}</li>
+      <li style="background:${colorSec.innerText}" class="color__name">${colorSec.innerText}</li>
+      `;
+      break;
+  }
+  return liTag;
+}
+
 //get random color function
+
 function randomColor() {
   return Math.floor(Math.random() * colors.length);
 }
+
 /*DELETE OR DEMO THE COLLECTED COLOR*/
 collectList.forEach((list) =>
   list.addEventListener("click", (e) => {
     let collectionGroup = e.target.parentElement;
     let li1 = collectionGroup.firstElementChild;
     let li2 = li1.nextElementSibling;
+    let li1Text = li1.innerText;
+    let li2Text = li2.innerText;
     //delete color from collection
     if (e.target.classList.contains("btn-trash")) {
       collectionGroup.classList.add("fade__away");
@@ -131,30 +136,67 @@ collectList.forEach((list) =>
       collectionGroup.addEventListener("transitionend", () => {
         collectionGroup.remove();
       });
+
       /*REMOVE COLOR FORM localStorage also*/
-      removeLocalColor(li1.innerText, li2.innerText);
+      removeLocalColor(li1Text, li2Text);
+      //show the new last color
+      checkBgColor(e.target.dataset.target, "normal");
     }
     //try back other color
     else if (e.target.classList.contains("btn-use")) {
       if (e.target.dataset.target == "linear") {
-        document.body.style.background = `linear-gradient(to right,${li1.innerText}, ${li2.innerText})`;
+        document.body.style.background = `linear-gradient(to right,${li1Text}, ${li2Text})`;
         //show the primary and secondary color
-        colorPri.innerText = li1.innerText;
-        colorSec.innerText = li2.innerText;
+        colorPri.innerHTML = li1Text;
+        colorSec.innerHTML = li2Text;
       } else {
-        document.body.style.background = li1.innerText;
+        document.body.style.background = li1Text;
         //show the current color
-        color.innerText = li1.innerText;
+        color.innerHTML = li1Text;
       }
     }
   })
 );
 
+/*SELECT MODE TO CHANGE THE DISPLAY MODE*/
+//choose mode to display onchange of select box
+function chooseMode(e) {
+  // checkLocalStorage();
+  contentGroup.forEach((content) => {
+    let dataMode = content.dataset.mode;
+    if (e.target.value == dataMode) {
+      content.classList.add("content-show");
+    } else {
+      content.classList.remove("content-show");
+      // console.log(colorSecArr);
+      // console.log(colorPriArr);
+      console.log(colorArr);
+    }
+    //set background onchange
+    checkBgColor(e.target.value, "normal-mode");
+  });
+}
+//display the last added color of each mode
+function checkBgColor(valueMode, compareMode) {
+  if (valueMode === compareMode) {
+    //change bgColor
+    document.body.style.background = colorArr.slice(-1)[0];
+    //change text info
+    color.innerHTML = colorArr.slice(-1)[0];
+  } else {
+    document.body.style.background = `linear-gradient(to right,${
+      colorPriArr.slice(-1)[0]
+    }, ${colorSecArr.slice(-1)[0]})`;
+
+    colorPri.innerHTML = colorPriArr.slice(-1)[0];
+    colorSec.innerHTML = colorSecArr.slice(-1)[0];
+  }
+}
+
 /*SAVE THE ADDED COLOR TO LOCALSTORAGE*/
 //@param: string: color normal, color pri, color sec, element: element use to distinguish wich mode u are on.
 function saveColorToLocal(hexColor, hexColorPri, hexColorSec, element) {
-  //check if there are any in localStorge
-  let [colorArr, colorPriArr, colorSecArr] = checkLocalStorage();
+  checkLocalStorage();
   /* To make sure we dont add Pri and Sec color in to theirs Array when in normal mode and vice versa. Push color to its relative array*/
   if (element.dataset.target != "linear") {
     colorArr.push(hexColor);
@@ -162,7 +204,7 @@ function saveColorToLocal(hexColor, hexColorPri, hexColorSec, element) {
     colorPriArr.push(hexColorPri);
     colorSecArr.push(hexColorSec);
   }
-
+  /*set Initial aray in local storage*/
   localStorage.setItem("colorArr", JSON.stringify(colorArr));
   localStorage.setItem("colorPriArr", JSON.stringify(colorPriArr));
   localStorage.setItem("colorSecArr", JSON.stringify(colorSecArr));
@@ -170,94 +212,47 @@ function saveColorToLocal(hexColor, hexColorPri, hexColorSec, element) {
 
 /*GET THE DATA FROM LOCALSTORAGE TO SHOW IN UI AFTER REFRESH PAGE*/
 function getLocalColor() {
-  //check if there are any in localStorge
-  let [colorArr, colorPriArr, colorSecArr] = checkLocalStorage();
-
+  checkLocalStorage();
   // Get color for normal mode
+  let contentHTML = "";
   colorArr.forEach((colorName) => {
-    //create div tag
-    let div = document.createElement("div");
-    div.classList.add("collection__group");
-    //create li tag
-    let li1 = document.createElement("li");
-    li1.classList.add("color__name");
+    contentHTML = `
+    <div class="collection__group">
+      <li class="color__name" style="background:${colorName}">${colorName}</li>
+      <li></li>
+      <button class="btn btn-trash" title="Delete" data-target="normal"></button>
+      <button class="btn btn-use" title="Demo this color" data-target="normal"></button>
+    </div>
+  `;
 
-    //create button trash tag
-    let button_del = document.createElement("button");
-    button_del.setAttribute("class", "btn btn-trash");
-    button_del.setAttribute("title", "Delete");
-    //create button demo
-    let button_demo = document.createElement("button");
-    button_demo.setAttribute("class", "btn btn-use");
-    button_demo.setAttribute("title", "Demo this color");
-
-    //Add tags together
-    div.appendChild(li1);
-    // div.appendChild(li2);
-    div.appendChild(button_del);
-    div.appendChild(button_demo);
-    //create li2 tag(seccondary color)
-    let li2 = document.createElement("li");
     if (contentGroup[0].dataset.mode == "normal-mode") {
-      li1.textContent = colorName;
-      //khong add class cho li2 de width=0 (li1 co flex:1)
-      li2.textContent = "";
-      //change bg-color of li
-      li1.style.background = colorName;
-      collectList[0].appendChild(div);
+      collectList[0].innerHTML += contentHTML;
     }
-    // add li2 to div
-    div.insertBefore(li2, button_del);
   });
+
   // Get color for linear mode
-  colorPriArr.forEach((colorPriName) => {
-    //create div tag
-    let div = document.createElement("div");
-    div.classList.add("collection__group");
-    //create li tag
-    let li1 = document.createElement("li");
-    li1.classList.add("color__name");
-
-    //create button trash tag
-    let button_del = document.createElement("button");
-    button_del.setAttribute("class", "btn btn-trash");
-    button_del.setAttribute("title", "Delete");
-    //create button demo
-    let button_demo = document.createElement("button");
-    button_demo.setAttribute("class", "btn btn-use");
-    button_demo.setAttribute("title", "Demo this color");
-
-    //Add tags together
-    div.appendChild(li1);
-    // div.appendChild(li2);
-    div.appendChild(button_del);
-    div.appendChild(button_demo);
-    //create li2 tag(seccondary color)
-    let li2 = document.createElement("li");
+  colorPriArr.forEach((colorPriName, index) => {
+    contentHTML = `
+      <div class="collection__group">
+        <li class="color__name" style="background:${colorPriName}">${colorPriName}</li>
+        <li class="color__name" style="background:${colorSecArr[index]}">${colorSecArr[index]}</li>
+        <button class="btn btn-trash" title="Delete" data-target="linear"></button>
+        <button class="btn btn-use" title="Demo this color" data-target="linear"></button>
+      </div>
+    `;
     if (contentGroup[1].dataset.mode == "linear-mode") {
-      li1.textContent = colorPriName;
-      li1.style.background = colorPriName;
-      button_demo.setAttribute("data-target", "linear");
-      button_del.setAttribute("data-target", "linear");
-      colorSecArr.some((colorSecName) => {
-        li2.textContent = colorSecName;
-        li2.classList.add("color__name");
-        li2.style.background = colorSecName;
-        colorSecArr.shift();
-        return true;
-      });
-
-      collectList[1].appendChild(div);
+      collectList[1].innerHTML += contentHTML;
     }
-    // add li2 to div
-    div.insertBefore(li2, button_del);
   });
+
+  //show the latest bg after load
+  checkBgColor(contentGroup[0].dataset.mode, "normal-mode");
 }
 
 /*REMOVE COLOR FROM LOCALSTORAGE WHEN CLICK DELETE*/
 //@param: string, string : color want to del
 function removeLocalColor(colorLi1, colorLi2) {
-  let [colorArr, colorPriArr, colorSecArr] = checkLocalStorage();
+  checkLocalStorage();
   let colorIndexLi1;
   let colorIndexLi2;
   contentGroup.forEach((content) => {
@@ -277,7 +272,6 @@ function removeLocalColor(colorLi1, colorLi2) {
       colorPriArr.splice(colorIndexLi1, 1);
       //update colorPriArr to localStorage after remove color
       localStorage.setItem("colorPriArr", JSON.stringify(colorPriArr));
-
       colorIndexLi2 = colorSecArr.indexOf(colorLi2);
       colorSecArr.splice(colorIndexLi2, 1);
       //update colorSecArr to localStorage after remove color
@@ -288,10 +282,10 @@ function removeLocalColor(colorLi1, colorLi2) {
 
 //Small func to check if the local storage have anything inside
 //output: 3 arrays (from localStorage) contain colors with its relative mode
+let colorArr;
+let colorPriArr;
+let colorSecArr;
 function checkLocalStorage() {
-  let colorArr;
-  let colorPriArr;
-  let colorSecArr;
   /*CHECK IF THE LOCALSTORAGE IS AVAILABLE, if there is, parse it back to its relative array*/
   if (localStorage.getItem("colorArr") === null) {
     colorArr = [];
@@ -310,22 +304,6 @@ function checkLocalStorage() {
   } else {
     colorSecArr = JSON.parse(localStorage.getItem("colorSecArr"));
   }
-  return [colorArr, colorPriArr, colorSecArr];
-}
 
-/*SELECT MODE TO CHANGE THE DISPLAY MODE*/
-//choose mode to display onchange of select box
-function chooseMode(e) {
-  contentGroup.forEach((content) => {
-    let dataMode = content.dataset.mode;
-    if (e.target.value == dataMode) {
-      content.classList.add("content-show");
-      //For Inspect view to know which mode is on
-      document.body.dataset.target = "linear";
-    } else {
-      content.classList.remove("content-show");
-      //For Inspect view
-      document.body.dataset.target = "normal";
-    }
-  });
+  return [colorArr, colorPriArr, colorSecArr];
 }
